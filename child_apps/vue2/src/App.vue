461f8242dd4beb2cfcd8a8e6e352e0243c529545
canlong.shen <562172151@qq.com>
2022-08-23 18:03:53 +0800

    feat: 新增 vue2 项目

---
+++ child_apps/vue2/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">这是vue2的项目</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+</style>
---
+++ child_apps/vue2/src/routes/router.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import RootApp from "@/root-app.vue";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: window.__MICRO_APP_BASE_ROUTE__ || "/",
+    component: RootApp,
+    children: [
+      // 其他的路由都写到这里
+      //   { path: "test1", component: () => import("@/views/page-test1.vue") },
+      //   { path: "test2", component: () => import("@/views/page-test2.vue") },
+    ],
+  },
+];
+
+const router = new VueRouter({
+  mode: "hash",
+  routes,
+});
+
+export default router;
