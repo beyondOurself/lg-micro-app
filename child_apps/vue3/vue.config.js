ed36e5b5dc3a4c91d15feb11ae6b403885a84921
canlong.shen <562172151@qq.com>
2022-09-01 20:07:42 +0800

    feat: 新增项目 应用配置

--- child_apps/vue3/vue.config.js
+++ child_apps/vue3/vue.config.js
@@ -8,4 +8,9 @@ module.exports = defineConfig({
     },
     port: "8082",
   },
+
+  // > 上线配置
+  outputDir: `../../dist/${process.env.VUE_APP_V3_NAME}`,
+  publicPath: `/${process.env.VUE_APP_V3_NAME}/`,
+  // < 上线配置
 });
